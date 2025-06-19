a31b410d615971cd58d73548b9ae4a40f806a93a
Acul Mathyas <mathyas.acul@stud.ubbcluj.ro>
2025-06-19 15:31:48 +0300

    Add files via upload

---
+++ Anul 1/Arhitectura Sistemelor de Calcul/Laboratoare/Lab 3/Adunări, scăderi/Cu semn/23.asm
@@ -0,0 +1,44 @@
+bits 32
+
+
+global start        
+
+extern exit              
+import exit msvcrt.dll
+extern printf    
+import printf msvcrt.dll
+
+segment data use32 class=data
+    print_frmt db "Rezultatul este %d", 0
+    a db 1
+    b dw 2
+    c dd 3
+    d dq 4
+    
+segment code use32 class=code
+    start:
+    xor EAX, EAX
+    xor EBX, EBX
+    xor EDX, EDX
+    
+    mov al, [a]
+    cbw
+    add ax, [b]
+    cwde
+    add eax, [c]
+    cdq
+    add eax, [d]
+    
+    mov bl, [a]
+    cbw
+    add bx, [b]
+        
+    sbb eax, ebx
+    push eax
+    push print_frmt 
+    call [printf]
+        
+    add  esp, 4 * 2
+
+    push    dword 0      
+    call    [exit]   
